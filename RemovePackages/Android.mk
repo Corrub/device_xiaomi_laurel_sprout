b7a4df050491099c6f6ee1cfe50d059d5ec4fc15
Mohan Nagesh <mohan.nagesh1@gmail.com>
2023-06-23 09:31:00 +0530

    Initial commit

---
+++ RemovePackages/Android.mk
@@ -0,0 +1,25 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := RemovePackages
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_OVERRIDES_PACKAGES := \
+	ANGLE \
+	BetterBugStub \
+	AmbientSensePrebuilt \
+	AppDirectedSMSService \
+	CalculatorGooglePrebuilt \
+	CalendarGooglePrebuilt \
+	Camera2 \
+	Camera2Stub \
+	CalendarGooglePrebuilt \
+	CarrierLocation \
+	DeviceIntelligenceNetworkPrebuilt \
+	PlayAutoInstallConfig \
+	Velve
+
+LOCAL_UNINSTALLABLE_MODULE := true
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := /dev/null
+include $(BUILD_PREBUILT)
---
+++ aosp_laurel_sprout.mk
@@ -0,0 +1,46 @@
+#
+# Copyright (C) 2021 The Evolution-X Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+
+# Inherit some common Pixel stuff
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# Inherit from laurel_sprout device
+$(call inherit-product, $(LOCAL_PATH)/laurel_sprout.mk)
+
+PRODUCT_BRAND := Xiaomi
+PRODUCT_DEVICE := laurel_sprout
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_NAME := aosp_laurel_sprout
+PRODUCT_MODEL := Mi A3
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# ABI Checks
+SKIP_ABI_CHECKS := true
+
+# Props
+#EVO_BUILD_TYPE := OFFICIAL
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_GAPPS_ARCH := arm64
+TARGET_SUPPORTS_QUICK_TAP := true
+#TARGET_USES_MINI_GAPPS := true
+#EVO_SIGNED := true
